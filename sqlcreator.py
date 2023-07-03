63f30ed224f623193a8e73c079816eddc073017f
AshishRam7 <jaashishram7@gmail.com>
2023-07-03 20:27:08 +0530

    Files uploaded

---
+++ sqlcreator.py
@@ -0,0 +1,11 @@
+# import sqlite3
+import app
+
+print(app.tstock)
+
+# conn = sqlite3.connect('Sales.db')
+# print("Opened database successfully");
+
+# conn.execute('INSERT TABLE orders(id INT, cid INT, name TEXT, quantity INT)')
+# print("Table created successfully");
+# conn.close()
